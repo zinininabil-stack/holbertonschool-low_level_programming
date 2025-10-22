0d7337f55292cb15c6378a90406ee91ad25affcb
Nab Z <zinini.nabil@gmail.com>
2025-10-22 13:26:33 +0000

    Pushed prog not the .c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -15,11 +15,11 @@ int last_digit;
 	n = rand() - RAND_MAX / 2;
 last_digit = n % 10;
 {
-if (last_digit== 0)
+if (last_digit == 0)
 printf("Last digit of %d is %d and is 0\n", n, last_digit);
 else if (last_digit > 5)
 printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
-if (last_digit< 6)
+else
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
 
 }
