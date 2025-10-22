b8e6963be47886725065723c7b1dec17c43377fd
Nab Z <zinini.nabil@gmail.com>
2025-10-22 09:01:32 +0000

    Fix: return to a new line

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
+
 /**
  * main - Entry point
  *
@@ -13,11 +13,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-	printf("%d is positive", n);
+	printf("%d is positive\n", n);
 	if (n == 0)
-	printf("%d is zero", n);
+	printf("%d is zero\n", n);
 	if (n < 0)
-	printf("%d is negative", n);
+	printf("%d is negative\n", n);
 
 	return (0);
 }
