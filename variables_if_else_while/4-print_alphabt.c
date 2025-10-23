0c7fbdef4c9126a35dcf81aac59ab43023d188d1
Nab Z <zinini.nabil@gmail.com>
2025-10-23 10:09:54 +0000

    Alphabet printed without some letters

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+	if (c == 'e')
+	{
+		continue;
+	}
+	else if (c == 'q')
+	{
+		continue;
+	}
+	else
+	{
+		putchar(c);
+	}
+
+putchar('\n');
+return (0);
+}
