9e43dbc528978b0f9965fadda02219206480b28d
Nab Z <zinini.nabil@gmail.com>
2025-10-24 11:14:16 +0000

    sdf

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -7,8 +7,8 @@
 int main(void)
 
 {
-	{
-	_putchar(95);
+
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
@@ -16,7 +16,7 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	}
+
 
 
 	_putchar('\n');
