01365a59db841b3d7743d9a3f51d0fefd3aff67c
Nab Z <zinini.nabil@gmail.com>
2025-10-24 12:41:41 +0000

    print alphabet lowercase

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void) /*Alphabet lowercase function*/
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		{
+		_putchar(c);
+		}
+		_putchar('\n');
+
+}
