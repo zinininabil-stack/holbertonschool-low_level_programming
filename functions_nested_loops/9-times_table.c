bbec2f296adcbb19aa445021ffa88ad94ae2b0a6
Nab Z <zinini.nabil@gmail.com>
2025-10-26 20:28:39 +0000

    Print timetable of 1 to 9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * times_table - check the code
+ *
+ * Return: Always 0.
+ */
+
+void times_table(void)
+{
+
+	int a = 0;
+	int b = 0;
+	int x = 0;
+
+	for (a = 0 ; a < 10 ; a++)
+	{
+		_putchar('0');
+
+		for (b = 1 ; b < 10 ; b++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			x = (a * b);
+
+			if (x <= 9)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((x / 10) + '0');
+			}
+			_putchar((x % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
