ba723f0e0a7c7ada1da28f9be873db562d25572a
Nab Z <zinini.nabil@gmail.com>
2025-10-26 17:22:45 +0000

    Printing depending on the sign numbers

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @n: the number to check
+ *
+ * This function prints '+' if n is positive, '0' if n is zero,
+ * and '-' if n is negative.
+ *
+ * Return: 1 if n is positive, 0 if n is zero, -1 if n is negative
+ */
+
+int print_sign(int n)
+
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
