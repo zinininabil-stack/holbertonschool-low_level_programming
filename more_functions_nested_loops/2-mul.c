24ef16ba4fed12f88d00eb47c2e836d33b2be363
Nab Z <zinini.nabil@gmail.com>
2025-10-27 08:26:22 +0000

    Function multiplies a and b

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies a and b
+ * @a: first number
+ * @b: second number
+ * Description: This function multiplies a and b
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
