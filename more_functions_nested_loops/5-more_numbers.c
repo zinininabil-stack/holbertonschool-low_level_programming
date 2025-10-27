d56ac51e63acf67c467c6e05d5895c8f61def6ea
Nab Z <zinini.nabil@gmail.com>
2025-10-27 09:52:13 +0000

    Print first 14 digit 10 times

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints numbers from 0 to 14, 10 times
+ *
+ * Description: Uses nested loops to print numbers 0 to 14
+ * ten times, each followed by a newline. Only uses _putchar
+ * three times.
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int c, d;
+
+	for (c = 0; c <= 10; c++)
+	{
+		for (d = 0; d <= 14; d++)
+		{
+			if (d >= 10)
+			_putchar('1');
+			_putchar((d % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
