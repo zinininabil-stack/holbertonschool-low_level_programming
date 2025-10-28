9a4feea2255beb586ffb252d6323b9e72e148974
Nab Z <zinini.nabil@gmail.com>
2025-10-28 08:37:08 +0000

    Fizz Buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Prints the numbers from 1 to 100 with FizzBuzz rules
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n; /** Numbers */
+for (n = 1; n <= 100; n++)
+{
+	if (n % 5 == 0 && n % 3 == 0)
+	printf("FizzBuzz");
+
+	else if (n % 5 == 0)
+	printf("Buzz");
+
+	else if (n % 3 == 0)
+	printf("Fizz");
+
+	else
+		printf("%d" , n);
+}
+	printf("\n");
+
+	return (0);
+}
