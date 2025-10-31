ca848b22ce2e0d9e79d063964ee301fd100d7bd0
zinininabil-stack <zinini.nabil@gmail.com>
2025-10-31 15:43:07 +0100

    Implement cap_string function to capitalize the first letter of each word in a string

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,26 +1,38 @@
 #include "main.h"
 
 /**
- * string_toupper - Converts all lowercase letters of a string to uppercase
- * @a: Pointer to the string to be converted
+ * cap_string - capitalizes all words of a string
+ * @str: input string
  *
- * Description: This function iterates through the string pointed to by @a
- *              and converts all lowercase alphabetic characters to their
- *              uppercase equivalents. The conversion is done in place.
+ * This function converts the first letter of each word in a string
+ * to uppercase. Words are considered to be separated by spaces,
+ * punctuation marks, or newline/tab characters.
  *
- * Return: Pointer to the modified string
+ * Return: pointer to the resulting string
  */
 
-char *string_toupper(char *a)
-
+char *cap_string(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (a[i] != '\0')
+	if (str[0] >= 97 && str[0] <= 122)
+		{
+		str[0] = (str[0] - 32);
+		}
+
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (a[i] >= 'a' && a[i] <= 'z')
-		a[i] = a[i] - 32;
-		i++;
+		if (str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+			str[i] == '!' || str[i] == '?' || str[i] == '"' ||
+			str[i] == '(' || str[i] == ')' || str[i] == '{' ||
+			str[i] == '}' || str[i] == 32 || str[i] == '\t' ||
+			str[i] == '\n')
+		{
+			if (str[i + 1] >= 97 && str[i + 1] <= 122)
+			{
+			str[i + 1] = (str[i + 1] - 32);
+			}
+		}
 	}
-	return (a);
+	return (str);
 }
