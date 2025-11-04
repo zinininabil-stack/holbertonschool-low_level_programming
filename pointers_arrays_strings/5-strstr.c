1de817148660153d452d8eb9bba2723f1b3b4a36
zinininabil-stack <zinini.nabil@gmail.com>
2025-11-04 15:01:03 +0100

    Add _strstr function to locate a substring and implement chessboard printing

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: string to be scanned
+ * @needle: substring to search for
+ *
+ * Return: pointer to the beginning of the located substring,
+ * or NULL if the substring is not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	if (needle[0] == '\0')
+		return (haystack);
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i + j] != needle[j])
+				break;
+		}
+		if (needle[j] == '\0')
+			return (&haystack[i]);
+	}
+
+	return (NULL);
+}
