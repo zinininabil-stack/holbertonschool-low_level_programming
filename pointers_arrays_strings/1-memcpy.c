7ffc2c42e56bdd6b48db83663c8f380dbaab5c4e
Nab Z <zinini.nabil@gmail.com>
2025-11-04 10:44:49 +0100

    Copies memory area

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _memcpy - copies memory area
+ * @dest: destination memory area
+ * @src: source memory area
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+unsigned int i;
+{	
+for (i = 0; i < n; i++)
+	{
+		src[i] = dest[i];
+	}
+
+
+	return(dest);
+}
