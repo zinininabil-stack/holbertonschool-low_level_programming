04f5945bc509c4ddd8141b832c5a5e4206c21c81
zinininabil-stack <zinini.nabil@gmail.com>
2025-11-04 11:14:35 +0100

    Function copying memory area

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -8,13 +8,13 @@
  * Return: pointer to dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
-unsigned int i = 0
-{	
-for (i = 0; i < n; i++)
+{
+unsigned int i;
+	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
 	}
 
 
-	return(dest);
+	return (dest);
 }
