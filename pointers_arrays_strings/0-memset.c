801ff0b9819833f644d32a1525cf64bd0f9628a0
Nab Z <zinini.nabil@gmail.com>
2025-11-04 09:53:00 +0100

    Fills memory with constant byte

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+
+
+*/
+char *_memset(char *s, char b, unsigned int n)
+
+{
+	
+	unsigned int n = 0
+	{
+		while (i < n)
+		s[i] = b
+		i++;
+	}
+
+	return (s);
+
+}
