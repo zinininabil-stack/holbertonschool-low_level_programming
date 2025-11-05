0bf29ac84ffc112ba198ad534e7564e703f0d6e4
zinininabil-stack <zinini.nabil@gmail.com>
2025-11-05 13:38:54 +0100

    Refactor _pow_recursion function to correctly calculate power of x raised to y

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,16 +1,16 @@
-#include "main.h"
-#include <stdio.h>
-
 /**
- * main - check the code
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: base integer
+ * @y: exponent integer
  *
- * Return: Always 0.
+ * Return: the value of x raised to the power of y
+ *         -1 if y is lower than 0
  */
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
-
-	return (x * y);
-
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
 }
