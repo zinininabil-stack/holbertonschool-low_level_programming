60a677874c9481422e86f042ac068f511ddec7d2
zinininabil-stack <zinini.nabil@gmail.com>
2025-11-06 14:59:58 +0100

    Refactor main function in 0-whatsmyname.c to remove unnecessary loop and print only the program name

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -7,12 +7,10 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
-
-	for (i = 0; i <= argc; i++)
+	(void)argc;
 	{
-		printf("argv[%d] = %s\n", i, argv[i]);
+		printf("%s\n", argv[0]);
 	}
-
 	return (0);
 }
+
