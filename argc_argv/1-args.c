80137acc467094115202bb40bd9bde2ea938f353
zinininabil-stack <zinini.nabil@gmail.com>
2025-11-06 15:05:06 +0100

    Add initial implementation of 1-args.c to print the number of arguments

---
+++ argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - prints the name of the program
+ *
+ * @argc: number of arguments (not used)
+ * @argv: array of arguments (argv[0] is the program name)
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	{
+		printf("%d\n", argc);
+	}
+	return (0);
+}
