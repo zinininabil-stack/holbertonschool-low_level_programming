9938ab55afd9ada49f283fb19e261191c2c7f85a
zinininabil-stack <zinini.nabil@gmail.com>
2025-11-06 15:02:13 +0100

    Update comments in 0-whatsmyname.c for clarity and consistency

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,9 +1,12 @@
 #include "main.h"
 
 /**
+ * main - prints the name of the program
  *
+ * @argc: number of arguments (not used)
+ * @argv: array of arguments (argv[0] is the program name)
  *
- *
+ * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
@@ -13,4 +16,3 @@ int main(int argc, char *argv[])
 	}
 	return (0);
 }
-
