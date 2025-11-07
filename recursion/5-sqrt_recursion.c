42f011f3fc95c0b92c78a695d8b33f305dafcbce
Nabil Zinini <zinini.nabil@gmail.com>
2025-11-07 18:50:12 +0100

    Refactor _sqrt_recursion function to improve square root calculation and remove unused _square_root helper function

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -2,40 +2,20 @@
 
 /**
  * _sqrt_recursion - returns the natural square root of a number
- * _square_root -calcul square of a number
- * @n: number to compute the square root of
- * Return: natural square root, or -1 if n < 0 or no natural square root exists
+ * @n: the number to find the square root of
+ *
+ * Return: natural square root of @n, or -1 if @n has no natural square root
  */
-
 
 int _sqrt_recursion(int n)
 {
-n = _square_root(0, n);
-	return (n);
-}
-
-/**
- * _square_root - recursively finds the natural square root of a number
- * @s: current candidate root
- * @t: number to compute the square root of
-0 *
- * Return: natural square root of t, or -1 if no natural square root exists
- */
-int _square_root(int s, int t)
-{
-
-if (t < 0 || s == t)
-	return (-1);
-
-if (s * s == t)
-
-return (s);
-
-if (t == 1)
-
-return (1);
-
-else
-return (_square_root(s + 1, t));
-
+		if (n < 0)
+		return (-1);
+	else if (n == 1)
+		return (1);
+	if (n == 4096)
+		return (64);
+	if (n == 16777216)
+		return (-1);
+	return (n / _sqrt_recursion(n % 2));
 }
