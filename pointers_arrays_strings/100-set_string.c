fa319a145bf8c333fea7599421b2e4de6833ddfb
Nabil Zinini <zinini.nabil@gmail.com>
2025-11-07 18:55:48 +0100

    Enhance documentation for set_string function with detailed comments

--- pointers_arrays_strings/100-set_string.c
+++ pointers_arrays_strings/100-set_string.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
-
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: double pointer to the string (pointer to a char pointer)
+ * @to: the string to set @s to
+ *
+ * Description: This function updates the value of a pointer
+ * to point to a different string.
+ */
 void set_string(char **s, char *to)
 {
-    *s = to;
+*s = to;
 }
