985ba907bdf8e7ca8aec13828b2a902a3a6206a0
Nabil Zinini <zinini.nabil@gmail.com>
2025-11-10 10:27:12 +0100

    Refactor create_array function documentation and improve code clarity

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,23 +1,27 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+
 /**
- * create_array - Creates an array of chars and initializes it with a char.
- * @size: The size of the array to be created.
- * @c: The character to initialize the array with.
+ * create_array - Creates an array of chars and initializes it with a specific char
+ * @size: The size of the array to be created
+ * @c: The character to initialize the array with
  *
- * Return: A pointer to the array, or NULL if it fails or if size is 0.
+ * Return: Pointer to the array, or NULL if it fails or if size is 0
  */
 char *create_array(unsigned int size, char c)
 {
-int i = 0;
+	char *array;
+	unsigned int i;
 
-		while (i < size)
-		printf("size[i]%s\n", i);
-		if size = 0
+	if (size == 0)
 		return (NULL);
 
-		if (size == '/0')
-		(return (*s) || return (NULL));
+	array = malloc(sizeof(char) * size);
+	if (array == NULL)
+		return (NULL);
 
+	for (i = 0; i < size; i++)
+		array[i] = c;
+
+	return (array);
 }
