728c09f7da25ea5023773c8d9a74660064bccf79
Nabil Zinini <zinini.nabil@gmail.com>
2025-11-10 09:57:19 +0100

    Add create_array function to allocate and initialize an array of chars

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * create_array - Creates an array of chars and initializes it with a char.
+ * @size: The size of the array to be created.
+ * @c: The character to initialize the array with.
+ *
+ * Return: A pointer to the array, or NULL if it fails or if size is 0.
+ */
+char *create_array(unsigned int size, char c)
+{
+int i = 0;
+
+		while (i < size)
+		printf("size[i]%s\n", i);
+		if size = 0
+		return (NULL);
+
+		if (size == '/0')
+		(return (*s) || return (NULL));
+
+}
