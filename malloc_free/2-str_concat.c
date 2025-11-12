285b0d6abb597d8e83d6870007e3b73b0ec4ff4e
zinininabil-stack <zinini.nabil@gmail.com>
2025-11-12 10:37:50 +0100

    Add str_concat function to concatenate two strings and update header file

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - concatenates two strings into a newly allocated memory space
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: pointer to a newly allocated string containing s1 followed by s2,
+ * or NULL if memory allocation fails
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *concat;
+	unsigned int i, j, len1 = 0, len2 = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[len1] != '\0')
+		len1++;
+
+	while (s2[len2] != '\0')
+		len2++;
+
+	concat = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (concat == NULL)
+		return (NULL);
+
+	for (i = 0; i < len1; i++)
+		concat[i] = s1[i];
+
+	for (j = 0; j < len2; j++)
