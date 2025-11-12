8aa11372511c100b7e912b7f92578b6948ee5427
zinininabil-stack <zinini.nabil@gmail.com>
2025-11-12 12:49:36 +0100

    Add alloc_grid function to allocate a 2D grid of integers and update header file

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - allocates a 2D grid of integers
+ * @width: width of the grid
+ * @height: height of the grid
+ *
+ * Return: pointer to the grid, or NULL on failure
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **grid;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	grid = malloc(sizeof(int *) * height);
+	if (grid == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(grid[j]);
+			free(grid);
+			return (NULL);
+		}
+	}
+
+	return (grid);
+}
