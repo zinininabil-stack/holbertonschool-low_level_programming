172289a5d855ec1b748722c10feecdd18c51fa72
Nabil Zinini <zinini.nabil@gmail.com>
2025-11-17 17:00:29 +0100

    Add arithmetic operation functions for addition, subtraction, multiplication, division, and modulus

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+
+/**
+ * op_add - Adds two integers.
+ * @a: First integer.
+ * @b: Second integer.
+ *
+ * Return: The sum of a and b.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Subtracts the second integer from the first.
+ * @a: First integer.
+ * @b: Second integer.
+ *
+ * Return: The difference of a and b.
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Multiplies two integers.
+ * @a: First integer.
+ * @b: Second integer.
+ *
+ * Return: The product of a and b.
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Divides the first integer by the second.
+ * @a: First integer.
+ * @b: Second integer.
+ *
+ * Return: The result of the division of a by b.
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Computes the remainder of the division of two integers.
+ * @a: First integer.
+ * @b: Second integer.
+ *
+ * Return: The remainder of the division of a by b.
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
