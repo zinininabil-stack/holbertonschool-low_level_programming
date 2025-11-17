a134d776383f5e6480b2e1fdbb06ccd4b1ddbddf
zinininabil-stack <zinini.nabil@gmail.com>
2025-11-17 11:44:56 +0100

    Fix array_iterator function formatting and improve null checks

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 #include "function_pointers.h"
+
 /**
+ * array_iterator - executes a function on each element of an array
+ * @array: pointer to the array of integers
+ * @size: number of elements in the array
+ * @action: pointer to the function to use
  *
- *
+ * Return: Nothing.
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
+	if (array == NULL || action == NULL)
+		return;
 
-	for (i = 0, i < size, i++)
+	for (i = 0; i < size; i++)
 		action(array[i]);
 }
