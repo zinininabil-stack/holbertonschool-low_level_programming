f11b9228ca48696fedebfdd63a36104a88cf9af6
zinininabil-stack <zinini.nabil@gmail.com>
2025-12-01 12:49:59 +0100

    Fixr list_len function to remove print functionality and count elements in list

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,30 +1,21 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
- * print_list - Prints all the elements of a list_t list.
- * @h: Pointer to the head of the list_t list.
+ * list_len - Returns the number of elements in a linked list_t list
+ * @h: Pointer to the head of the list
  *
- * Description: This function loops through each node of a singly
- * linked list and prints the string stored in the node. If the
- * string is NULL, it prints "[0] (nil)". It returns the total
- * number of nodes in the list.
+ * Description: Loops through the linked list and counts each node.
  *
- * Return: The number of nodes in the list.
+ * Return: The total number of nodes in the list
  */
-size_t list_len(const list_t *h);
+size_t list_len(const list_t *h)
 {
 	size_t count = 0;
 
 	while (h != NULL)
 	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
-		else
-			printf("[%u] %s\n", h->len, h->str);
-
-		h = h->next;
 		count++;
+		h = h->next;
 	}
 
 	return (count);
