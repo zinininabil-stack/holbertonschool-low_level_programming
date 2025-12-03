27295ed1eb4274d1830f27c8cab20ea7c04cddf0
zinininabil-stack <zinini.nabil@gmail.com>
2025-12-03 11:27:19 +0100

    Refactor add_node function to use standard strdup and remove custom string functions

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,59 +1,45 @@
 #include "lists.h"
 #include <stdlib.h>
-
-/* Custom string length function */
-unsigned int my_strlen(const char *s)
-{
-	unsigned int len = 0;
-
-	while (s[len] != '\0')
-		len++;
-
-	return (len);
-}
-
-/* Custom strdup() implementation */
-char *my_strdup(const char *s)
-{
-	unsigned int i, len = my_strlen(s);
-	char *copy = malloc(len + 1);
-
-	if (!copy)
-		return (NULL);
-
-	for (i = 0; i < len; i++)
-		copy[i] = s[i];
-
-	copy[len] = '\0';
-	return (copy);
-}
+#include <string.h>
 
 /**
- * add_node - Adds a new node at the beginning of a linked list
- * @head: Pointer to the pointer to the head of the list
- * @str: String to store in the new node
+ * add_node - Adds a new node at the beginning of a list_t list
+ * @head: Pointer to the head pointer of the list
+ * @str: String to duplicate into the new node
  *
- * Return: Address of the new node, or NULL on failure
+ * Return: Address of the new node, or NULL if it failed
+ *
+ * Description: The function allocates a new node, duplicates the string
+ * using strdup, calculates the length manually, and inserts the node
+ * at the beginning of the linked list.
  */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
+	unsigned int len = 0;
 
 	if (!head || !str)
 		return (NULL);
 
+	/* Allocate memory for new node */
 	new_node = malloc(sizeof(list_t));
 	if (!new_node)
 		return (NULL);
 
-	new_node->str = my_strdup(str);
+	/* Duplicate string */
+	new_node->str = strdup(str);
 	if (!new_node->str)
 	{
 		free(new_node);
 		return (NULL);
 	}
 
-	new_node->len = my_strlen(str);
+	/* Compute length manually */
+	while (str[len])
+		len++;
+	new_node->len = len;
+
+	/* Insert at the beginning */
 	new_node->next = *head;
 	*head = new_node;
 
